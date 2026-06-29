4e63be3bb801573b39430e4b27f37878751900cc
Subham Agarwal <subhamaga04@gmail.com>
2026-06-29 10:21:51 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+
+        for(char ch : s){
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch); 
+            }else if(ch == ')'){
+                if(!st.empty() && st.top() == '(') st.pop(); 
+                else return false; 
+            }else if(ch == '}'){
+                if(!st.empty() && st.top() == '{') st.pop(); 
+                else return false; 
+            }else if(ch == ']'){
+                if(!st.empty() && st.top() == '[') st.pop(); 
+                else return false; 
+            }
+        } 
+
+        return st.empty(); 
+    }
+};
