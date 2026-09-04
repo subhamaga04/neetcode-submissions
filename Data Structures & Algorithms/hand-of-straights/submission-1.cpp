036981bcece2692e774e2af9685d0e6f9f590636
Subham Agarwal <subhamaga04@gmail.com>
2026-09-04 19:08:19 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0) return false; 
+        map<int, int> mp; 
+
+        for(int it : hand){
+            mp[it]++; 
+        }
+
+        while(!mp.empty()){
+            int start = mp.begin()->first;
+
+            for(int i = 0; i < groupSize; i++){
+                int card = start + i; 
+
+                if(mp.find(card) == mp.end()) return false; 
+
+                mp[card]--; 
+
+                if(mp[card] == 0){
+                    mp.erase(card); 
+                }
+            }
+        }  
+        return true; 
+    }
+};
